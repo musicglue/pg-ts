1caa2153ac966acfdc3220b46b34750d29839647
Niklas Lindgren <niklas@musicglue.com>
2017-11-13 14:22:25 +0000

    feat: add SQLUnsafeRaw template tag
    Allows embedding a dynamic non-bound value in an SQL query

--- src/utils/sql.ts
+++ src/utils/sql.ts
@@ -7,12 +7,18 @@ export const SQLFragment = (parts: TemplateStringsArray, ...inValues: any[]) => 
 ): string =>
   parts.reduce((prev, curr, valIdx) => `${prev}${getValueIndex(inValues[valIdx - 1])}${curr}`);
 
+export const SQLUnsafeRaw = (parts: TemplateStringsArray, ...inValues: string[]) => (..._: any[]) =>
+  parts.reduce((prev, curr, valIdx) => `${prev}${inValues[valIdx - 1]}${curr}`);
+
 export const SQL = (parts: TemplateStringsArray, ...inValues: any[]): pg.QueryConfig => {
   const outValues: any[] = [];
 
   const getValueIndex = (value: any): string => {
     if (value == null) {
       return `NULL`;
+    }
+    if (typeof value === "function") {
+      return `${value()}`;
     }
     const found = outValues.indexOf(value);
     if (found > -1) {
