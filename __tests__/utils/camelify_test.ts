761269ce24d5bcadd88625bc057f9d39a2829757
Niklas Lindgren <niklas@musicglue.com>
2017-12-29 12:06:48 +0000

    test: add more test cases for arrays

--- __tests__/utils/camelify_test.ts
+++ __tests__/utils/camelify_test.ts
@@ -2,15 +2,32 @@ import camelify from "../../src/utils/camelify";
 
 describe("Utils", () => {
   describe("Camelisation", () => {
-    it("ignores strings", () => {
-      expect(camelify("hello_world")).toEqual("hello_world");
+    describe("Scalars pass through unaltered", () => {
+      it("strings", () => expect(camelify("hello_world")).toEqual("hello_world"));
+      it("numbers", () => expect(camelify(123)).toEqual(123));
+      it("booleans", () => expect(camelify(false)).toEqual(false));
     });
 
-    it("ignores arrays", () => {
-      const candidate = ["hello_world", "hello_world"];
-      const result = camelify(candidate);
-      expect(result).toBeInstanceOf(Array);
-      expect(result).toEqual(candidate);
+    describe("Array keys are unaltered", () => {
+      it("does not alter array keys", () => {
+        const candidate = ["hello_world", "hello_world"];
+        const result = camelify(candidate);
+        expect(result).toBeInstanceOf(Array);
+        expect(result).toEqual(candidate);
+      });
+
+      it("does not alter nested array keys", () => {
+        const candidate = [["beep", ["boop"]], ["this", "that"]];
+        expect(camelify(candidate)).toEqual(candidate);
+      });
+
+      it("camelcases objects in arrays", () =>
+        expect(camelify([{ beep_boop: "beepBoop" }])).toEqual([{ beepBoop: "beepBoop" }]));
+
+      it("camelcases objects in nested arrays", () =>
+        expect(
+          camelify([[{ this_that: "thisThat" }, { beep_boop: [{ beep_bzzt: "beepBzzt" }] }]]),
+        ).toEqual([[{ thisThat: "thisThat" }, { beepBoop: [{ beepBzzt: "beepBzzt" }] }]]));
     });
 
     it("camelises simple objects", () => {
