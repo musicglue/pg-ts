6546268dbf9b12135154846a877407e389c82042
Lee Henson <lee.m.henson@gmail.com>
2018-03-19 11:40:44 +0000

    refactor: remove connection string parser, in favour of pg-connection-string

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 export { makePool } from "./pool";
 export { queryAny, queryNone, queryOne, queryOneOrMore, queryOneOrNone } from "./query";
-export { parseConnectionString } from "./utils/connection";
 export { SQL, SQLFragment } from "./utils/sql";
